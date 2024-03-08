622fa06843112622554b2206fb1615c73d526e42
Braden <43161720+Raiza31@users.noreply.github.com>
2024-03-07 15:46:33 -1000

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "biKWD59haC9g70EeX9wLLXlqfnmNi1XnMcbUBwiYIRog9QUJ9a4nwZnEeaa8KxRi";
 /**
  * Get list of teams in event
  *
