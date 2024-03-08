bfe9cc078bcc8fe34547e2b480eda308cea8f355
Braden <43161720+Raiza31@users.noreply.github.com>
2024-03-07 16:10:43 -1000

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "20203hiho",
+      "defaultValue": "2023hiho",
       "required": "true"
     },
     { "name": "Match Level",
